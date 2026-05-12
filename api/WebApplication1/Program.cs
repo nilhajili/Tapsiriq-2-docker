aebd0bf018bbdda570c69114e20cdecfe19e4437
nilhajili <hacilinilufer@gmail.com>
2026-05-12 16:31:16 +0400

    initial commit

--- api/WebApplication1/Program.cs
+++ api/WebApplication1/Program.cs
@@ -38,12 +38,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowFrontend");
 
